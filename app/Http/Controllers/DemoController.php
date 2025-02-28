0113e07753a2ff04b1ec3eb8259ba94ff4e7d00d
Foyez Ahamed <foyez1435@gmail.com>
2025-02-28 20:39:01 +0600

    working with file upload

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -9,19 +9,11 @@ class DemoController extends Controller
     function demoAction (Request $request){
         $photoFile=$request->file('photo');
 
-        $fileSize=filesize($photoFile);
-        $fileType=filetype($photoFile);
-        $fileTempName=$photoFile->getFilename();
-        $fileOriginalName=$photoFile->getClientOriginalName();
-        $fileExtension=$photoFile->extension();
+        // $photoFile->storeAs('upload', $photoFile->getClientOriginalName());
 
-        return array(
-            'photoFile'=>$photoFile,
-            'fileSize'=>$fileSize,
-            'fileType'=>$fileType,
-            'fileTempName'=>$fileTempName,
-            'fileOriginalName'=>$fileOriginalName,
-            'fileExtension'=>$fileExtension
-        );
+        $photoFile->move('upload', $photoFile->getClientOriginalName());
+
+        //ami chile eikhane upload/images o dite pari //
+
     }
 }
